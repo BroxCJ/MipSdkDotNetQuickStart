e6b725e2e2c708842e4a4a8ff207e3fa5f24239e
Joaquin Campo Brox <JCampo@joaquincampobrox.es>
2020-04-06 15:47:30 +0200

    Add project files.

---
+++ MipSdkDotNetQuickStart/Classes/AuthDelegateImplementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.InformationProtection;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace MipSdkDotNetQuickStart
+{
+    public class AuthDelegateImplementation : IAuthDelegate
+    {
+        private ApplicationInfo _appInfo;
+        private string redirectUri = "mip-sdk-app://authorize";
+        public AuthDelegateImplementation(ApplicationInfo appInfo)
+        {
+            _appInfo = appInfo;
+        }
+
+        public string AcquireToken(Identity identity, string authority, string resource, string claims)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            var result = Task.Run(async () => await authContext.AcquireTokenAsync(resource, _appInfo.ApplicationId, new Uri(redirectUri), new PlatformParameters(PromptBehavior.Always))).Result;
+            return result.AccessToken;
+        }
+    }
+    
+}
---
+++ MipSdkDotNetQuickStart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.InformationProtection;
+using Microsoft.InformationProtection.Exceptions;
+using Microsoft.InformationProtection.File;
+using Microsoft.InformationProtection.Protection;
+
+namespace MipSdkDotNetQuickStart
+{
+    class Program
+    {
+        private const string clientId = "<application-id>";
+        private const string appName = "<friendly-name>";
+
+        static void Main(string[] args)
+        {
+            //Initialize Wrapper for File API operations 
+            MIP.Initialize(MipComponent.File);
+        }
+    }
+}
